77323b4d127458b7938bd615ff0f3e9f7801bfce
kouchao <admin@jskou.com>
2018-10-31 10:37:34 +0800

    refactor: remove element-ui

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,14 +27,12 @@ module.exports = {
   ** Global CSS
   */
   css: [
-    'element-ui/lib/theme-chalk/index.css'
   ],
 
   /*
   ** Plugins to load before mounting the App
   */
   plugins: [
-    '@/plugins/element-ui'
   ],
 
   /*
